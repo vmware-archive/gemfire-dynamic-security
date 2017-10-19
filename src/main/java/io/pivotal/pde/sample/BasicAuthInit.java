ce6d6654a03049dc463d3b990e8c82f3998b2c42
wrmay <rmay@pivotal.io>
2017-10-19 16:22:59 -0400

    added a BasicAuthInit for use by clients

---
+++ src/main/java/io/pivotal/pde/sample/BasicAuthInit.java
@@ -0,0 +1,36 @@
+package io.pivotal.pde.sample;
+import java.util.Properties;
+
+import org.apache.geode.LogWriter;
+import org.apache.geode.distributed.DistributedMember;
+import org.apache.geode.security.AuthInitialize;
+import org.apache.geode.security.AuthenticationFailedException;
+
+public class BasicAuthInit implements AuthInitialize {
+	
+	private LogWriter systemLogger, securityLogger;
+		
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public Properties getCredentials(Properties props, DistributedMember distrMember,
+			boolean isPeer) throws AuthenticationFailedException {
+		String username = props.getProperty("security-username");
+		String password = props.getProperty("security-password");
+		
+		Properties result = new Properties();
+		result.setProperty("security-username", username);
+		result.setProperty("security-password", password);
+		return result;
+	}
+
+	@Override
+	public void init(LogWriter systemLogger, LogWriter securityLogger)
+			throws AuthenticationFailedException {
+		this.systemLogger = systemLogger;
+		this.securityLogger = securityLogger;
+	}
+
+}
